7fbd4c9bfc8c59af47d7e2d106010d9f930dfa9a
Nick <nbaugh1@gmail.com>
2023-02-08 17:19:35 -0500

    return app client

--- app.js
+++ app.js
@@ -33,6 +33,11 @@ app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/notes', notesRouter);
 
+app.get('/notes*', (_, res) => {
+  res.sendFile(path.join(__dirname, 'public') + '/index.html');
+});
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
